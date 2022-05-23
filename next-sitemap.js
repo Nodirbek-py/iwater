4fe3a1bbb8c349857937037a96918712473b895a
nodirbek-00 <nodirbek.vositov@hygge.software>
2022-05-23 18:37:03 +0500

    FEAT: Sitemap is created

---
+++ next-sitemap.js
@@ -0,0 +1,14 @@
+const siteUrl = "https://iwater.com/";
+
+module.exports = {
+  siteUrl,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+  },
+};
